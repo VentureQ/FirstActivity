e29b3645655d9c191796a516cf09351d8ae1269c
venture_git <wqldd0607@163.com>
2022-06-16 12:47:31 +0800

    RelativeLayoutActivity

--- app/src/main/java/com/example/firstactivity/LinearLayoutActivity.java
+++ app/src/main/java/com/example/firstactivity/LinearLayoutActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LayoutActivity extends AppCompatActivity {
+public class LinearLayoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_layout);
+        setContentView(R.layout.activity_linear_layout);
     }
 }
